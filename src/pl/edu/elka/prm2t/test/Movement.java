7b0f5337361c99606a237a842a96856246420880
ktrzaskoma <kttrzaskoma@gmail.com>
2022-04-16 11:55:38 +0200

    Stworzenie nowego brancha! NIE MERGOWAC!

---
+++ src/pl/edu/elka/prm2t/test/Movement.java
@@ -0,0 +1,6 @@
+package pl.edu.elka.prm2t.test;
+
+interface Movement {
+
+    void move(int toX, int toY);
+}
---
+++ src/pl/edu/elka/prm2t/test/MovementImpl.java
@@ -0,0 +1,8 @@
+package pl.edu.elka.prm2t.test;
+
+class MovementImpl implements Movement{
+    @Override
+    public void move(int toX, int toY) {
+
+    }
+}
